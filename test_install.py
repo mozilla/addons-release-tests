c3b521e896c13a6076fcd9937ce509d927319d12
Alexandra Gal-Moga <amoga@mozilla.com>
2021-01-22 12:32:46 +0200

    Update test_install.py
    Skipping installs until Nightly can be used again (Nightly is currently affected by bug https://bugzilla.mozilla.org/show_bug.cgi?id=1686708)

--- test_install.py
+++ test_install.py
@@ -1,7 +1,7 @@
 import pytest
 from pages.desktop.details import Detail
 
-
+@pytest.mark.skip(reason="Skipping installs until nightly can be used again")
 @pytest.mark.desktop_only
 @pytest.mark.nondestructive
 @pytest.mark.parametrize('addon_type, name_type', [
