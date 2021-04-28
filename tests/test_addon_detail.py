8694a5908f63851402d2a39766b19d7dcddc0a0e
AlexandraMoga <amoga@mozilla.com>
2021-04-28 09:48:28 +0300

    Remove import that wasn't used

--- tests/test_addon_detail.py
+++ tests/test_addon_detail.py
@@ -1,7 +1,6 @@
 import pytest
 
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common import by
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expected
 
