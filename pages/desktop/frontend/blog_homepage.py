b5641150d69d3ffdd63a4007dced4109ada4afe7
cristian.bujoreanu <>
2022-02-21 15:57:41 +0200

    add page objects and tests for blog homepage

---
+++ pages/desktop/frontend/blog_homepage.py
@@ -0,0 +1,43 @@
+from pypom import Region
+from selenium.webdriver.common.by import By
+
+from pages.desktop.base import Base
+
+
+class BlogHomepage(Base):
+
+    _articles_locator = (By.CSS_SELECTOR, '.blog-entry')
+
+    @property
+    def articles(self):
+        items = self.find_elements(*self._articles_locator)
+        return [self.ArticlesList(self, el) for el in items]
+
+    class ArticlesList(Region):
+
+        _image_locator = (By.TAG_NAME, 'img')
+        _image_link_locator = (By.CSS_SELECTOR, '.blog-entry-featured-image')
+        _title_locator = (By.CLASS_NAME, 'blog-entry-title')
+        _date_locator = (By.CLASS_NAME, 'blog-entry-date')
+        _intro_text_locator = (By.CSS_SELECTOR, '.blog-entry-excerpt > p:nth-child(1)')
+        _read_more_link_locator = (By.CSS_SELECTOR, '.blog-entry-read-more > a')
+
+        @property
+        def image(self):
+            return self.find_element(*self._image_locator)
+
+        @property
+        def title(self):
+            return self.find_element(*self._title_locator)
+
+        @property
+        def date(self):
+            return self.find_element(*self._date_locator)
+
+        @property
+        def intro_text(self):
+            return self.find_element(*self._intro_text_locator)
+
+        @property
+        def read_more_link(self):
+            return self.find_element(*self._read_more_link_locator)
