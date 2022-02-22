29f1c0a168bd4d2e90e16a480e1c469dbef05589
cristian0995 <95921687+cristian0995@users.noreply.github.com>
2022-02-22 11:26:19 +0200

    Addons homepage blog (#448)
    * add base_url for production into prod.json
    
    * add page objects and tests for blog homepage
    
    * add page objects and tests for blog homepage
    
    * reformat with black and fix mistake
    
    * apply PR requested changes
    
    * apply PR requested changes
    
    Co-authored-by: cristian.bujoreanu <>

---
+++ tests/frontend/test_blog_homepage.py
@@ -0,0 +1,57 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+from pages.desktop.frontend.blog_homepage import BlogHomepage
+from pages.desktop.frontend.home import Home
+
+
+@pytest.mark.nondesstructive
+def test_header_logo_button(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page.header.click_title()
+    homepage = Home(selenium, base_url)
+    assert homepage.primary_hero.is_displayed()
+
+
+@pytest.mark.nondesstructive
+def test_articles_elements_are_displayed(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    for article in page.articles:
+        assert article.image.is_displayed()
+        assert article.title.is_displayed()
+        assert article.date.is_displayed()
+        assert article.intro_text.is_displayed()
+        assert article.read_more_link.is_displayed()
+
+
+@pytest.mark.nondesstructive
+def test_open_article_by_clicking_article_image(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    article_title = page.articles[0].title.text
+    page.articles[0].image.click()
+    assert (
+        article_title.lower()
+        in page.find_element(By.CLASS_NAME, 'header-title').text.lower()
+    )
+
+
+@pytest.mark.nondesstructive
+def test_open_article_by_clicking_article_title(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    article_title = page.articles[0].title.text
+    page.articles[0].title.click()
+    assert (
+        article_title.lower()
+        in page.find_element(By.CLASS_NAME, 'header-title').text.lower()
+    )
+
+
+@pytest.mark.nondesstructive
+def test_open_article_by_clicking_read_more_link(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    article_title = page.articles[0].title.text
+    page.articles[0].read_more_link.click()
+    assert (
+        article_title.lower()
+        in page.find_element(By.CLASS_NAME, 'header-title').text.lower()
+    )
