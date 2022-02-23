82f44ba697136ce8dc0094d19cf0ef747556f25d
cristian0995 <95921687+cristian0995@users.noreply.github.com>
2022-02-23 14:20:50 +0200

    add page objects and tests for blog article pages (#450)
    * add page objects and tests for blog article pages
    
    * apply PR requests
    
    * unite blog_homepage.py and blog_article.py into blog.py
    
    Co-authored-by: cristian.bujoreanu <>

---
+++ pages/desktop/frontend/blog.py
@@ -0,0 +1,188 @@
+from pypom import Region
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+from pages.desktop.base import Base
+
+
+class BlogHomepage(Base):
+    URL_TEMPLATE = 'blog/'
+
+    _articles_locator = (By.CSS_SELECTOR, '.blog-entry')
+
+    @property
+    def articles(self):
+        items = self.find_elements(*self._articles_locator)
+        return [self.ArticlesList(self, el) for el in items]
+
+    class ArticlesList(Region):
+
+        _image_locator = (By.CSS_SELECTOR, '.blog-entry-featured-image > img')
+        _image_link_locator = (By.CSS_SELECTOR, '.blog-entry-featured-image')
+        _title_locator = (By.CLASS_NAME, 'blog-entry-title')
+        _date_locator = (By.CLASS_NAME, 'blog-entry-date')
+        _intro_text_locator = (By.CSS_SELECTOR, '.blog-entry-excerpt > p:nth-child(1)')
+        _read_more_link_locator = (By.CSS_SELECTOR, '.blog-entry-read-more > a')
+
+        @property
+        def image(self):
+            return self.find_element(*self._image_locator)
+
+        @property
+        def title(self):
+            return self.find_element(*self._title_locator)
+
+        @property
+        def date(self):
+            return self.find_element(*self._date_locator)
+
+        @property
+        def intro_text(self):
+            return self.find_element(*self._intro_text_locator)
+
+        @property
+        def read_more_link(self):
+            return self.find_element(*self._read_more_link_locator)
+
+        def click_read_more_link(self):
+            self.read_more_link.click()
+            return ArticlePage(self.driver, self.page.base_url)
+
+
+class ArticlePage(Base):
+    _header_logo_locator = (By.CLASS_NAME, 'header-logo')
+    _article_title = (By.CLASS_NAME, 'header-title')
+    _navigation_bar_locator = (By.CSS_SELECTOR, '.blogpost-breadcrumb ol li')
+    _content_paragraphs_locator = (By.CSS_SELECTOR, '.blogpost-content-wrapper > p')
+    _last_updated_date_locator = (By.CSS_SELECTOR, 'dd.updated')
+    _previous_article_link_locator = (By.CSS_SELECTOR, '.blogpost-nav-prev a p')
+    _next_article_link_locator = (By.CSS_SELECTOR, '.blogpost-nav-next a p')
+    _author_info_section_locator = (By.CLASS_NAME, 'blogpost-meta')
+    _static_addon_card_locator = (By.CLASS_NAME, 'StaticAddonCard')
+
+    def wait_for_page_to_load(self):
+        self.wait.until(
+            EC.visibility_of_element_located(self._author_info_section_locator)
+        )
+        return self
+
+    @property
+    def header_logo(self):
+        return self.find_element(*self._header_logo_locator)
+
+    @property
+    def title(self):
+        return self.find_element(*self._article_title)
+
+    @property
+    def nav_bar_links(self):
+        return self.find_elements(*self._navigation_bar_locator)
+
+    @property
+    def content_paragraphs(self):
+        return self.find_elements(*self._content_paragraphs_locator)
+
+    @property
+    def last_updated_date(self):
+        return self.find_element(*self._last_updated_date_locator)
+
+    @property
+    def next_article(self):
+        return self.find_element(*self._next_article_link_locator)
+
+    @property
+    def previous_article(self):
+        return self.find_element(*self._previous_article_link_locator)
+
+    @property
+    def author(self):
+        return self.Author(self, self.find_element(*self._author_info_section_locator))
+
+    class Author(Region):
+        _name_locator = (By.CSS_SELECTOR, 'dd.author')
+        _picture_locator = (By.CSS_SELECTOR, '.author-avatar > img')
+        _twitter_link_locator = (By.CLASS_NAME, 'share-twitter-link')
+        _pocket_link_locator = (By.CLASS_NAME, 'share-pocket-link')
+
+        @property
+        def name(self):
+            return self.find_element(*self._name_locator)
+
+        @property
+        def picture(self):
+            return self.find_element(*self._picture_locator)
+
+        @property
+        def twitter_link(self):
+            return self.find_element(*self._twitter_link_locator)
+
+        @property
+        def pocket_link(self):
+            return self.find_element(*self._pocket_link_locator)
+
+    @property
+    def addon_cards(self):
+        return [
+            self.AddonCard(self, el)
+            for el in self.find_elements(*self._static_addon_card_locator)
+        ]
+
+    class AddonCard(Region):
+        _title_locator = (By.CSS_SELECTOR, '.AddonTitle > a')
+        _author_locator = (By.CSS_SELECTOR, '.AddonTitle-author > a')
+        _summary_locator = (By.CLASS_NAME, 'StaticAddonCard-summary')
+        _rating_locator = (By.CLASS_NAME, 'Rating')
+        _users_number_locator = (By.CLASS_NAME, 'StaticAddonCard-metadata-adu')
+        _add_to_firefox_button_locator = (By.CLASS_NAME, 'GetFirefoxButton-button')
+        _recommended_badge_link_locator = (
+            By.CLASS_NAME,
+            'PromotedBadge-link--recommended',
+        )
+
+        @property
+        def title(self):
+            return self.find_element(*self._title_locator)
+
+        @property
+        def author(self):
+            return self.find_element(*self._author_locator)
+
+        @property
+        def summary(self):
+            return self.find_element(*self._summary_locator).text
+
+        @property
+        def rating(self):
+            return float(
+                self.find_element(*self._rating_locator)
+                .get_attribute('title')
+                .split()[1]
+            )
+
+        @property
+        def users_number(self):
+            return int(
+                self.find_element(*self._users_number_locator)
+                .text.split('Users: ')[1]
+                .replace(",", "")
+            )
+
+        @property
+        def add_to_firefox_button(self):
+            return self.find_element(*self._add_to_firefox_button_locator)
+
+        @property
+        def is_recommended(self):
+            try:
+                assert self.find_element(
+                    *self._recommended_badge_link_locator
+                ).is_displayed()
+                return True
+            except NoSuchElementException:
+                return False
+
+        @property
+        def recommended_link(self):
+            return self.find_element(*self._recommended_badge_link_locator)
---
+++ tests/frontend/test_blog.py
@@ -0,0 +1,154 @@
+import pytest
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from pages.desktop.frontend.blog import BlogHomepage, ArticlePage
+from pages.desktop.frontend.home import Home
+
+
+@pytest.mark.nondesstructive
+def test_blog_homepage_header_logo_button(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page.header.click_title()
+    homepage = Home(selenium, base_url)
+    assert homepage.primary_hero.is_displayed()
+
+
+@pytest.mark.nondesstructive
+def test_articles_elements_are_displayed(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    for article in page.articles:
+        assert article.image.is_displayed()
+        assert article.title.is_displayed()
+        assert article.date.is_displayed()
+        assert article.intro_text.is_displayed()
+        assert article.read_more_link.is_displayed()
+
+
+@pytest.mark.nondesstructive
+def test_open_article_by_clicking_article_image(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    article_title = page.articles[0].title.text
+    page.articles[0].image.click()
+    article_page = ArticlePage(selenium, base_url)
+    assert article_title.lower() in article_page.title.text.lower()
+
+
+@pytest.mark.nondesstructive
+def test_open_article_by_clicking_article_title(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    article_title = page.articles[0].title.text
+    page.articles[0].title.click()
+    article_page = ArticlePage(selenium, base_url)
+    assert article_title.lower() in article_page.title.text.lower()
+
+
+@pytest.mark.nondesstructive
+def test_open_article_by_clicking_read_more_link(base_url, selenium):
+    page = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    article_title = page.articles[0].title.text
+    page.articles[0].read_more_link.click()
+    article_page = ArticlePage(selenium, base_url)
+    assert article_title.lower() in article_page.title.text.lower()
+
+
+@pytest.mark.nondestructive
+def test_article_page_loaded_correctly(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    assert page.header_logo.is_displayed()
+    assert page.title.is_displayed()
+    assert page.author.name.is_displayed()
+    assert page.author.picture.is_displayed()
+    assert page.author.twitter_link.is_displayed()
+    assert page.author.pocket_link.is_displayed()
+    for link in page.nav_bar_links:
+        assert link.is_displayed()
+    # verify that the article has at least one paragraph
+    assert page.content_paragraphs[0].is_displayed()
+    # verify that at least one of next/prev article links is displayed
+    try:
+        assert page.next_article.is_displayed()
+    except NoSuchElementException:
+        page.previous_article.is_displayed()
+
+
+@pytest.mark.nondestructive
+def test_article_page_header_logo_button(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    page.header_logo.click()
+    homepage = Home(selenium, base_url)
+    assert homepage.primary_hero.is_displayed()
+
+
+@pytest.mark.nondestructive
+def test_navbar_frontend_homepage_link(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    page.nav_bar_links[0].click()
+    homepage = Home(selenium, base_url)
+    assert homepage.primary_hero.is_displayed()
+
+
+@pytest.mark.nondestructive
+def test_navbar_blog_homepage_link(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    page.nav_bar_links[1].click()
+    blog_homepage = BlogHomepage(selenium, base_url)
+    assert blog_homepage.articles[0].title.is_displayed()
+
+
+@pytest.mark.nondestructive
+def test_navbar_current_article_link(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    initial_title = page.title.text
+    page.nav_bar_links[2].click()
+    assert initial_title in page.title.text
+
+
+@pytest.mark.nondestructive
+def test_next_and_previous_article_links(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    initial_title = page.title.text
+    page.next_article.click()
+    assert initial_title not in page.title.text
+    page.previous_article.click()
+    assert initial_title in page.title.text
+
+
+@pytest.mark.nondestructive
+def test_addon_cards_loaded_correctly(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    for card in page.addon_cards:
+        assert card.title.is_displayed()
+        assert card.author.is_displayed()
+        assert len(card.summary) > 0
+        assert 0 <= card.rating <= 5
+        assert card.users_number >= 0
+        assert card.add_to_firefox_button.is_displayed()
+
+
+@pytest.mark.nondestructive
+def test_addon_card_recommendation_badge_link(base_url, selenium, variables):
+    blog_homepage = BlogHomepage(selenium, base_url).open().wait_for_page_to_load()
+    page = blog_homepage.articles[0].click_read_more_link()
+    for addon_card in page.addon_cards:
+        if addon_card.is_recommended:
+            addon_card.recommended_link.click()
+            initial_window = page.driver.window_handles[0]
+            child_window = page.driver.window_handles[1]
+            page.driver.switch_to.window(child_window)
+            page.wait.until(
+                EC.visibility_of_element_located((By.CLASS_NAME, 'sumo-page-heading'))
+            )
+            assert (
+                'Add-on Badges'
+                in page.driver.find_element(By.CLASS_NAME, 'sumo-page-heading').text
+            )
+            page.driver.switch_to.window(initial_window)
