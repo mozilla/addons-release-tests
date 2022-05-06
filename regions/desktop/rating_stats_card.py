f7ce148b504c59b83a8371321cd57612dbab4eda
cristian0995 <95921687+cristian0995@users.noreply.github.com>
2022-05-06 10:35:03 +0300

    add page objects for versions page and review card (#515)
    * add page objects for versions page and review card
    
    * apply PR requests
    
    Co-authored-by: cristian.bujoreanu <>

---
+++ regions/desktop/rating_stats_card.py
@@ -0,0 +1,61 @@
+from pypom import Region
+from selenium.webdriver.common.by import By
+
+
+class RatingStats(Region):
+
+    _addon_title_locator = (By.CSS_SELECTOR, '.AddonTitle > a')
+    _addon_image_locator = (By.CSS_SELECTOR, '.AddonSummaryCard-header-icon-image')
+    _addon_author_locator = (By.CSS_SELECTOR, '.AddonTitle-author > a')
+    _addon_rating_locator = (By.CSS_SELECTOR, '.AddonSummaryCard-addonAverage')
+    _number_of_reviews_locator = (By.CLASS_NAME, 'RatingsByStar-count')
+    _rating_stars_locator = (By.CSS_SELECTOR, '.Rating-star')
+    _rating_by_star_locator = (By.CSS_SELECTOR, '.RatingsByStar-graph > a')
+
+    @property
+    def addon_title(self):
+        return self.find_element(*self._addon_title_locator)
+
+    def click_addon_title(self):
+        self.find_element(*self._addon_title_locator).click()
+        from pages.desktop.frontend.details import Detail
+
+        return Detail(self.selenium, self.page)
+
+    @property
+    def addon_image(self):
+        return self.find_element(*self._addon_image_locator)
+
+    def click_addon_image(self):
+        self.find_element(*self._addon_image_locator).click()
+        from pages.desktop.frontend.details import Detail
+
+        return Detail(self.selenium, self.page)
+
+    @property
+    def addon_author_names(self):
+        return [i.text for i in self.find_elements(*self._addon_author_locator)]
+
+    def click_author_name(self, index=0):
+        self.find_elements(*self._addon_author_locator)[index].click()
+        from pages.desktop.frontend.users import User
+
+        return User(self.selenium, self.page).view
+
+    @property
+    def rating_stars(self):
+        return self.find_element(*self._rating_stars_locator)
+
+    @property
+    def rating(self):
+        rating = self.find_element(*self._addon_rating_locator).text.split()[0]
+        return float(rating)
+
+    def click_see_all_reviews_with_specific_stars(self, count):
+        self.find_elements(*self._rating_by_star_locator)[count].click()
+        from pages.desktop.frontend.reviews import Reviews
+
+        return Reviews(self.selenium, self.page)
+
+    def number_of_reviews_with_specific_stars(self, count):
+        return int(self.find_elements(*self._number_of_reviews_locator)[count].text)
