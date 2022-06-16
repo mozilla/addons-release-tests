59d9d7e4f1e788b4770ee67f4902bd5bac337f0a
AlexandraMoga <amoga@mozilla.com>
2022-06-16 18:34:43 +0300

    Second set of tests for the addon submission API

---
+++ api/payloads.py
@@ -0,0 +1,144 @@
+def listed_addon_details(uuid):
+    body = {
+        'categories': {
+            'android': [
+                'experimental',
+                'performance'
+            ],
+            'firefox': [
+                'bookmarks',
+                'privacy-security'
+            ]
+        },
+        'slug': 'my_sluggish_slug',
+        'default_locale': 'en-US',
+        'name': {
+            'de': 'DE Name set at creation time',
+            'en-US': 'EN-US Name set at creation time',
+            'fr': 'FR Name set at creation time'
+        },
+        'summary': {
+            'de': 'Summary, in german',
+            'en-US': 'Summary, in en-US',
+            'fr': 'Summary, in french'
+        },
+        'description': {
+            'de': 'Description in german',
+            'en-US': 'Description in en-US',
+            'fr': 'Description in french'
+        },
+        'developer_comments': {
+            'de': 'Dev comments in german',
+            'en-US': 'Dev comments in en-US',
+            'fr': 'Dev comments in french'
+        },
+        'homepage': {
+            'de': 'https://addons.not-allizom.de',
+            'en-US': 'https://addons.not-allizom.org',
+            'fr': 'https://addons.not-allizom.fr'
+        },
+        'support_email': {
+            'de': 'lang-de@mail.com',
+            'en-US': 'lang-en-us@mail.com',
+            'fr': 'lang-fr@mail.com'
+        },
+        'is_experimental': True,
+        'requires_payment': True,
+        'contributions_url': 'https://www.patreon.com',
+        'tags': [
+            'anti malware',
+            'anti tracker',
+            'antivirus',
+            'chat',
+            'container',
+            'content blocker',
+            'coupon',
+            'dailymotion',
+            'dark mode'
+        ],
+        'version': {
+            'upload': uuid,
+            'license': 'all-rights-reserved',
+            'release_notes': {
+                'de': 'DE Version notes added in API at addon creation time',
+                'en-US': 'EN-US Version notes added in API at addon creation time',
+                'fr': 'FR Version notes added in API at addon creation time'
+            },
+            'compatibility': {
+                'android': {
+                    'min': '68.0',
+                    'max': '*'
+                },
+                'firefox': {
+                    'min': '58.0',
+                    'max': '100.*'
+                }
+            }
+        }
+    }
+    return body
+
+
+edit_addon_details = {
+    'categories': {
+        'android': [
+            'photos-media',
+            'shopping'
+        ],
+        'firefox': [
+            'appearance',
+            'download-management'
+        ]
+    },
+    'slug': 'new_sluggish_slug',
+    'name': {
+        'de': 'DE Name edited',
+        'en-US': 'EN-US Name edited',
+        'fr': 'FR Name edited',
+        'ro': 'RO Name edited'
+    },
+    'summary': {
+        'de': 'Summary, in german edited',
+        'en-US': 'Summary, in en-US edited',
+        'fr': 'Summary, in french edited',
+        'ro': 'Summary in romanian edited'
+    },
+    'description': {
+        'de': 'Description in german edited',
+        'en-US': 'Description in en-US edited',
+        'fr': 'Description in french edited',
+        'ro': 'Description in romanian edited'
+    },
+    'developer_comments': {
+        'de': 'Dev comments in german edited',
+        'en-US': 'Dev comments in en-US edited',
+        'fr': 'Dev comments in french edited',
+        'ro': 'Dev comments in romanian edited',
+    },
+    'homepage': {
+        'de': 'https://addons.edited-allizom.de',
+        'en-US': 'https://addons.edited-allizom.org',
+        'fr': 'https://addons.not-allizom.fr',
+        'ro': 'https://addons.not-allizom.ro'
+    },
+    'support_email': {
+        'de': 'lang-de-edited@mail.com',
+        'en-US': 'lang-en-us-edited@mail.com',
+        'fr': 'lang-fr@mail.com',
+        'ro': 'lang-ro@mail.com'
+    },
+    'is_experimental': False,
+    'requires_payment': False,
+    'contributions_url': 'https://donate.mozilla.org',
+    'tags': [
+        'dndbeyond',
+        'download',
+        'duckduckgo',
+        'facebook',
+        'google',
+        'mp3',
+        'music',
+        'password manager',
+        'pinterest'
+    ]
+}
--- tests/api/test_addon_uploads.py
+++ tests/api/test_addon_uploads.py
@@ -4,6 +4,7 @@ import time
 import pytest
 import requests
 
+from api import payloads, api_helpers
 from pages.desktop.frontend.home import Home
 
 # endpoints used in the upload tests
@@ -43,8 +44,8 @@ def test_upload_addon_without_dev_agreement(base_url, selenium):
     ), f'Actual response message was {upload.text}'
 
 
-@pytest.mark.login('api_user')
 @pytest.mark.serial
+@pytest.mark.login('api_user')
 def test_bad_authentication_addon_upload(selenium, base_url):
     upload = requests.post(
         url=f'{base_url}{_upload}',
@@ -61,8 +62,7 @@ def test_bad_authentication_addon_upload(selenium, base_url):
 
 @pytest.mark.serial
 @pytest.mark.create_session('api_user')
-@pytest.mark.clear_session
-def test_upload_unlisted_extension(base_url, selenium, session_auth):
+def test_upload_unlisted_extension(base_url, session_auth):
     upload = requests.post(
         url=f'{base_url}{_upload}',
         headers={"Authorization": f"Session {session_auth}"},
@@ -95,3 +95,118 @@ def test_upload_unlisted_extension(base_url, selenium, session_auth):
     # get the edit url for the add-on to verify that it was created and visible in devhub
     r = requests.get(resp['edit_url'], cookies={"sessionid": session_auth}, timeout=10)
     assert r.status_code == 200
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_submit_extension_with_invalid_uuid_format(base_url, session_auth):
+    """The UUID format doesn't match the expected format for the uuid value"""
+    uuid = ['some-invalid-uuid', {'upload': 'd4ce752a971b4a5aafcd175122726431'}, None, '']
+    for item in uuid:
+        data = {'version': {'upload': item}}
+        create_addon = requests.post(
+            url=f'{base_url}{_addon_create}',
+            headers={'Authorization': f'Session {session_auth}', 'Content-Type': 'application/json'},
+            data=json.dumps(data)
+        )
+        # capture the response details to ease debugging
+        print(f'For UUID "{item}": Response status is '
+              f'{create_addon.status_code}; {create_addon.text}\n')
+        # verify the status code and the response message for each value sent
+        assert create_addon.status_code == 400, f'Actual status code was {create_addon.status_code}'
+        if item is None or item == '':
+            assert 'This field may not be null.' \
+                   in create_addon.text, f'Actual response message was {create_addon.text}'
+        else:
+            assert f'“{item}” is not a valid UUID' in create_addon.text, \
+                f'Actual response was {create_addon.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_submit_extension_with_incorrect_uuid(base_url, session_auth):
+    """The UUID format is accepted but there is no valid upload found for the given uuid"""
+    uuid = ['d4ce752a971b4a5aafcd175122726431', 12345]
+    for item in uuid:
+        data = {'version': {'upload': item}}
+        create_addon = requests.post(
+            url=f'{base_url}{_addon_create}',
+            headers={'Authorization': f'Session {session_auth}', 'Content-Type': 'application/json'},
+            data=json.dumps(data)
+        )
+        print(f'For UUID "{item}": Response status is '
+              f'{create_addon.status_code}; {create_addon.text}\n')
+        assert create_addon.status_code == 400, f'Actual status code was {create_addon.status_code}'
+        assert f'Object with uuid={item} does not exist.' \
+               in create_addon.text, f'Actual response was {create_addon.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_upload_listed_extension(base_url, session_auth):
+    upload = requests.post(
+        url=f'{base_url}{_upload}',
+        headers={'Authorization': f'Session {session_auth}'},
+        files={'upload': open('sample-addons/listed-addon.zip', 'rb')},
+        data={'channel': 'listed'}
+    )
+    resp = upload.json()
+    print(json.dumps(resp, indent=2))
+    upload.raise_for_status()
+    assert 'listed' in resp['channel']
+    # get the addon uuid generated after upload
+    uuid = resp['uuid']
+    payload = payloads.listed_addon_details(uuid)
+    # sleep to allow the first request to be processed
+    time.sleep(5)
+    create_addon = requests.post(
+        url=f'{base_url}{_addon_create}',
+        headers={'Authorization': f'Session {session_auth}', 'Content-Type': 'application/json'},
+        data=json.dumps(payload)
+    )
+    create_addon.raise_for_status()
+    response = create_addon.json()
+    print(json.dumps(response, indent=2))
+    # verify that the data we sent has been registered correctly in the response we get
+    api_helpers.verify_addon_response_details(payload, response, 'create')
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_edit_listed_addon_details(base_url, session_auth):
+    payload = payloads.edit_addon_details
+    edit_addon = requests.patch(
+        url=f'{base_url}{_addon_create}my_sluggish_slug/',
+        headers={'Authorization': f'Session {session_auth}', 'Content-Type': 'application/json'},
+        data=json.dumps(payload)
+    )
+    edit_addon.raise_for_status()
+    response = edit_addon.json()
+    print(json.dumps(response, indent=2))
+    # verify that the data we sent has been registered correctly in the response we get
+    api_helpers.verify_addon_response_details(payload, response, 'edit')
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+@pytest.mark.clear_session
+def test_delete_extension_valid_token(selenium, base_url, session_auth, variables):
+    addon = payloads.edit_addon_details['slug']
+    get_delete_confirm = requests.get(
+        url=f'{base_url}{_addon_create}{addon}/delete_confirm/',
+        headers={'Authorization': f'Session {session_auth}'}
+    )
+    get_delete_confirm.raise_for_status()
+    r = get_delete_confirm.json()
+    token = r['delete_confirm']
+    delete_addon = requests.delete(
+        url=f'{base_url}{_addon_create}{addon}/',
+        headers={'Authorization': f'Session {session_auth}'},
+        params={'delete_confirm': token}
+    )
+    assert delete_addon.status_code == 204, f'Actual status code was {delete_addon.status_code}'
+    get_addon = requests.get(
+        url=f'{base_url}{_addon_create}{addon}/',
+        headers={'Authorization': f'Session {session_auth}'}
+    )
+    assert get_addon.status_code == 404, f'Actual status code was {get_addon.status_code}'
