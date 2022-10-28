268fb097bcc4b9dc2b909fa6400a06144cb782ea
Alexandra Gal-Moga <amoga@mozilla.com>
2022-10-28 14:05:25 +0300

    add more tests for additional authors and role permissions (#677)

--- tests/api/test_api_authors_edit.py
+++ tests/api/test_api_authors_edit.py
@@ -244,10 +244,7 @@ def test_addon_delete_pending_author(base_url, session_auth, variables):
 
 @pytest.mark.serial
 @pytest.mark.create_session('staff_user')
-@pytest.mark.clear_session
-def test_addon_author_confirm_deleted_invitation(
-    base_url, selenium, session_auth, variables
-):
+def test_addon_author_confirm_deleted_invitation(base_url, session_auth, variables):
     """With the author that was invited, try to accept the deleted invite to make sure it is not possible"""
     addon = payloads.edit_addon_details['slug']
     confirm_deleted_invite = requests.post(
@@ -257,3 +254,181 @@ def test_addon_author_confirm_deleted_invitation(
     assert (
         confirm_deleted_invite.status_code == 403
     ), f'Actual response: {confirm_deleted_invite.status_code}, {confirm_deleted_invite.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_addon_invite_multiple_authors(base_url, session_auth, variables):
+    """Check that an addon owner can invite multiple users to become addon authors
+    in addition to the one added previously"""
+    addon = payloads.edit_addon_details['slug']
+    # invite the first author
+    first_author = variables['api_post_valid_author']
+    payload = {**payloads.author_stats, 'user_id': first_author, 'position': 1}
+    second_invite = requests.post(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/',
+        headers={
+            'Authorization': f'Session {session_auth}',
+            'Content-Type': 'application/json',
+        },
+        data=json.dumps(payload),
+    )
+    assert (
+        second_invite.status_code == 201
+    ), f'Actual response: {second_invite.status_code}, {second_invite.text}'
+    # invite the second author
+    second_author = variables['api_post_additional_author']
+    payload = {**payloads.author_stats, 'user_id': second_author, 'position': 2}
+    second_invite = requests.post(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/',
+        headers={
+            'Authorization': f'Session {session_auth}',
+            'Content-Type': 'application/json',
+        },
+        data=json.dumps(payload),
+    )
+    assert (
+        second_invite.status_code == 201
+    ), f'Actual response: {second_invite.status_code}, {second_invite.text}'
+    # verify that both users are present in the pending authors list
+    get_pending_authors = requests.get(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/',
+        headers={'Authorization': f'Session {session_auth}'},
+    )
+    assert second_author == get_pending_authors.json()[0].get('user_id')
+    assert first_author == get_pending_authors.json()[1].get('user_id')
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_addon_invite_same_author_twice(base_url, session_auth, variables):
+    """Check that an author can only be invited once, if the invitation is still active"""
+    addon = payloads.edit_addon_details['slug']
+    author = variables['api_post_additional_author']
+    payload = {**payloads.author_stats, 'user_id': author, 'position': 2}
+    duplicate_invite = requests.post(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/',
+        headers={
+            'Authorization': f'Session {session_auth}',
+            'Content-Type': 'application/json',
+        },
+        data=json.dumps(payload),
+    )
+    assert (
+        duplicate_invite.status_code == 400
+    ), f'Actual response: {duplicate_invite.status_code}, {duplicate_invite.text}'
+    assert (
+        'An author can only be present once.' in duplicate_invite.text
+    ), f'Actual response message was {duplicate_invite.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('staff_user')
+def test_addon_confirm_invitation_with_correct_user(base_url, session_auth, variables):
+    addon = payloads.edit_addon_details['slug']
+    accept_invite = requests.post(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/confirm/',
+        headers={'Authorization': f'Session {session_auth}'},
+    )
+    assert (
+        accept_invite.status_code == 200
+    ), f'Actual response: {accept_invite.status_code}, {accept_invite.text}'
+    # try to re-confirm invitation to make sure only once it's possible and there are no unexpected errors
+    reconfirm_invite = requests.post(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/confirm/',
+        headers={'Authorization': f'Session {session_auth}'},
+    )
+    assert (
+        reconfirm_invite.status_code == 403
+    ), f'Actual response {reconfirm_invite.status_code}, {reconfirm_invite.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('staff_user')
+def test_addon_developer_role_cannot_add_authors(base_url, session_auth, variables):
+    """Check that an author with a 'developer' role doesn't have the rights to invite other authors
+    for an addon; only authors with 'owner' roles have the rights to invite other users"""
+    addon = payloads.edit_addon_details['slug']
+    author = variables['api_post_additional_author']
+    payload = {**payloads.author_stats, 'user_id': author, 'position': 3}
+    invite_author = requests.post(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/',
+        headers={
+            'Authorization': f'Session {session_auth}',
+            'Content-Type': 'application/json',
+        },
+        data=json.dumps(payload),
+    )
+    assert (
+        invite_author.status_code == 403
+    ), f'Actual response: {invite_author.status_code}, {invite_author.text}'
+    assert (
+        'You do not have permission to perform this action.' in invite_author.text
+    ), f'Actual message was {invite_author.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('staff_user')
+def test_addon_developer_role_cannot_edit_pending_author(
+    base_url, session_auth, variables
+):
+    """Check that an author with a 'developer' role doesn't have the rights to edit details
+    for other pending authors; only authors with 'owner' roles have the rights to make changes"""
+    addon = payloads.edit_addon_details['slug']
+    author = variables['api_post_additional_author']
+    payload = {'role': 'owner', 'listed': True}
+    edit_authors = requests.patch(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/{author}/',
+        headers={
+            'Authorization': f'Session {session_auth}',
+            'Content-Type': 'application/json',
+        },
+        data=json.dumps(payload),
+    )
+    assert (
+        edit_authors.status_code == 403
+    ), f'Actual response: {edit_authors.status_code}, {edit_authors.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('staff_user')
+@pytest.mark.clear_session
+def test_addon_developer_role_cannot_delete_pending_author(
+    base_url, selenium, session_auth, variables
+):
+    """Check that an author with a 'developer' role doesn't have the rights to delete other
+    pending authors; only authors with 'owner' roles have the rights to delete them"""
+    addon = payloads.edit_addon_details['slug']
+    author = variables['api_post_additional_author']
+    delete_author = requests.delete(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/{author}/',
+        headers={'Authorization': f'Session {session_auth}'},
+    )
+    assert (
+        delete_author.status_code == 403
+    ), f'Actual response: {delete_author.status_code}, {delete_author.text}'
+
+
+@pytest.mark.serial
+@pytest.mark.create_session('api_user')
+def test_addon_delete_all_active_and_pending_authors(base_url, session_auth, variables):
+    """As the addon owner, delete all additional authors (pending or active)"""
+    addon = payloads.edit_addon_details['slug']
+    active_author = variables['api_post_valid_author']
+    pending_author = variables['api_post_additional_author']
+    # delete active author (invitation accepted)
+    delete_active_author = requests.delete(
+        url=f'{base_url}{_addon_create}{addon}/authors/{active_author}/',
+        headers={'Authorization': f'Session {session_auth}'},
+    )
+    assert (
+        delete_active_author.status_code == 204
+    ), f'Actual response: {delete_active_author.status_code}, {delete_active_author.text}'
+    # delete the pending author (invitation not confirmed)
+    delete_pending_author = requests.delete(
+        url=f'{base_url}{_addon_create}{addon}/pending-authors/{pending_author}/',
+        headers={'Authorization': f'Session {session_auth}'},
+    )
+    assert (
+        delete_pending_author.status_code == 204
+    ), f'Actual response: {delete_pending_author.status_code}, {delete_pending_author.text}'
