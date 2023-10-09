efd75e44344c6cc9a7f5f3fe29022b4cd85c4ab0
aschek <aschek@mozilla.com>
2023-10-09 11:58:14 +0300

    add back api tests to commit workflow

--- tests/api/test_api_addons_edit.py
+++ tests/api/test_api_addons_edit.py
@@ -43,7 +43,7 @@ def test_upload_listed_extension(base_url, session_auth):
     uuid = resp["uuid"]
     payload = payloads.listed_addon_details(uuid)
     # sleep to allow the first request to be processed
-    time.sleep(5)
+    time.sleep(10)
     create_addon = requests.post(
         url=f"{base_url}{_addon_create}",
         headers={
