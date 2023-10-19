aa8ede34ab98ceb06102e63ca1dbd901bbab4597
aschek <aschek@mozilla.com>
2023-10-19 15:30:21 +0300

    add two tests for devhub coverage

--- pages/desktop/developers/manage_authors_and_license.py
+++ pages/desktop/developers/manage_authors_and_license.py
@@ -1,7 +1,4 @@
-import time
-
-import pytest
-from pypom import Region, Page
+from pypom import Page
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,6 +9,17 @@ class ManageAuthorsAndLicenses(Page):
     _radio_button_gnu_general_public_license_selector = (By.ID, 'id_builtin_1')
     _save_changes_button_selector = (By.CSS_SELECTOR, 'div.listing-footer button')
     _notification_box_success_selector = (By.CSS_SELECTOR, '.notification-box > h2:nth-child(1)')
+    _authors_text_selector = (By.CSS_SELECTOR, 'tbody:nth-child(1) > tr:nth-child(1) > th')
+    _licenses_text_selector = (By.CSS_SELECTOR, 'tbody:nth-child(1) > tr:nth-child(3) > th:nth-child(1) > span')
+    _license_agreement_selector = (By.CSS_SELECTOR, 'tbody:nth-child(1) > tr:nth-child(4) > th:nth-child(1) > span')
+    _license_agreement_checkbox = (By.ID, 'id_has_eula')
+    _please_specify_license_text_selector = (By.CSS_SELECTOR, '.eula > label:nth-child(1)')
+    _license_agreement_textbox_selector = (By.ID, 'id_eula_0')
+    _privacy_policy_selector = (By.CSS_SELECTOR, 'tbody:nth-child(1) > tr:nth-child(5) > th:nth-child(1) > span')
+    _privacy_policy_checkbox_selector = (By.ID, 'id_has_priv')
+    _please_specify_privacy_policy_text_selector = (By.CSS_SELECTOR, '.priv > label:nth-child(1)')
+    _privacy_policy_textbox_selector = (By.ID, 'id_privacy_policy_0')
+
 
     @property
     def radio_button_mozilla_public_license(self):
@@ -24,6 +32,46 @@ class ManageAuthorsAndLicenses(Page):
     @property
     def notification_box_success(self):
         return self.find_element(*self._notification_box_success_selector)
+
+    @property
+    def authors_text_element(self):
+        return self.find_element(*self._authors_text_selector)
+
+    @property
+    def license_text_element(self):
+        return self.find_element(*self._licenses_text_selector)
+
+    @property
+    def license_agreement_selector(self):
+        return self.find_element(*self._license_agreement_selector)
+
+    @property
+    def license_agreement_checkbox(self):
+        return self.find_element(*self._license_agreement_checkbox)
+
+    @property
+    def please_specify_license_text(self):
+        return self.find_element(*self._please_specify_license_text_selector)
+
+    @property
+    def license_agreeement_textbox(self):
+        return self.find_element(*self._license_agreement_textbox_selector)
+
+    @property
+    def privacy_policy(self):
+        return self.find_element(*self._privacy_policy_selector)
+
+    @property
+    def privacy_policy_checkbox(self):
+        return self.find_element(*self._privacy_policy_checkbox_selector)
+
+    @property
+    def please_specify_privacy_policy_text(self):
+        return self.find_element(*self._please_specify_privacy_policy_text_selector)
+
+    @property
+    def privacy_policy_textbox(self):
+        return self.find_element(*self._privacy_policy_textbox_selector)
 
     @staticmethod
     def open_manage_authors_and_licenses_page(selenium, base_url, addon):
