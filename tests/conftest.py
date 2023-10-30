34e39c1c694c5fb6b52c1f4ddb364f4ad715b008
aschek <aschek@mozilla.com>
2023-10-30 16:24:31 +0200

    push

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,7 +46,7 @@ def firefox_options(firefox_options, base_url, variables):
     # for prod installation tests, we do not need to set special prefs, so we
     # separate the browser set-up based on the AMO environments
     if base_url == "https://addons.mozilla.org":
-        firefox_options.add_argument("-foreground")
+        firefox_options.add_argument("-headless")
         firefox_options.log.level = "trace"
         firefox_options.set_preference(
             "extensions.getAddons.discovery.api_url",
