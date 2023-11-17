580d79e4781db9ce945e25b4d688a88812789880
aschek <139126091+alexandruschek@users.noreply.github.com>
2023-11-17 16:54:09 +0200

    Schek/update_prod_tests (#899)
    * add fix
    
    * replaced addon in use

--- tests/frontend/test_sanity.py
+++ tests/frontend/test_sanity.py
@@ -161,9 +161,9 @@ def test_about_addons_addon_cards_author_link(selenium, base_url, wait):
         lambda _: len([el.disco_addon_author for el in about_addons.addon_cards_items])
         >= 7
     )
-    disco_addon_name = about_addons.addon_cards_items[0].disco_addon_name.text
+    disco_addon_name = about_addons.addon_cards_items[3].disco_addon_name.text
     # clicking on the author link should open the addon detail page on AMO
-    amo_detail_page = about_addons.addon_cards_items[0].click_disco_addon_author()
+    amo_detail_page = about_addons.addon_cards_items[3].click_disco_addon_author()
     # checking that the expected detail page was opened
     wait.until(lambda _: disco_addon_name == amo_detail_page.name)
 
