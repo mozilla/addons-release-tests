513047145c83661bee52f9782c64541ea3df0af7
aschek <aschek@mozilla.com>
2024-02-02 16:02:00 +0200

    add fix for fe test

--- tests/frontend/test_search.py
+++ tests/frontend/test_search.py
@@ -300,7 +300,7 @@ def test_top_rated_recommended_addons_tc_id_c92462(base_url, selenium, variables
     page.search.search_for("")
     # verify if sort filter applied correctly
     for result in search_page.result_list.search_results:
-        assert getattr(result, "rating") > 4
+        assert getattr(result, "rating") >= 4.0
     # verify badge type
     results = search_page.result_list.search_results
     for result in results:
@@ -322,6 +322,7 @@ def test_top_rated_recommended_extensions_tc_id_c92462(base_url, selenium, varia
     for result in search_page.result_list.search_results:
         assert getattr(result, "rating") >= 1.0
     # verify that no themes are displayed
+    print(search_page.result_list.themes)
     assert len(search_page.result_list.themes) == 0
     # verify badge type
     results = search_page.result_list.search_results
