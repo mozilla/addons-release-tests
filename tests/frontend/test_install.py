4008c88555f177f6ea563379d9abb389c3138260
aschek <aschek@mozilla.com>
2024-03-18 13:25:57 +0200

    fix_for_dev

--- tests/frontend/test_install.py
+++ tests/frontend/test_install.py
@@ -13,10 +13,10 @@ def test_install_uninstall_extension_tc_id_c393003(
     selenium, base_url, firefox, firefox_notifications, wait
 ):
     """Open an extension detail page, install it and then uninstall it"""
-    selenium.get(f"{base_url}/addon/aarafow-molla-mantinch/")
+    selenium.get(f"{base_url}/addon/camuse-wallam-gormoisow/")
     addon = Detail(selenium, base_url).wait_for_page_to_load()
     amo_addon_name = addon.name
-    assert amo_addon_name == "aarafow-molla-mantinch"
+    assert amo_addon_name == "camuse-wallam-gormoisow"
     assert addon.is_compatible
     addon.install()
     firefox.browser.wait_for_notification(
@@ -51,7 +51,7 @@ def test_enable_disable_extension(
     selenium, base_url, firefox, firefox_notifications, wait
 ):
     """Open an extension detail page, install it, disable it from about:addons then enable it back in AMO"""
-    selenium.get(f"{base_url}/addon/aarafow-molla-mantinch/")
+    selenium.get(f"{base_url}/addon/permissionsv7/")
     addon = Detail(selenium, base_url).wait_for_page_to_load()
     addon.install()
     firefox.browser.wait_for_notification(
@@ -69,7 +69,7 @@ def test_enable_disable_extension(
     # verify that about:addons marks the extension as disabled -  (disabled) appended to addon name
     wait.until(
         lambda _: about_addons.installed_addon_name[0].text
-        == "aarafow-molla-mantinch (disabled)"
+        == "PermissionsOrder3 (disabled)"
     )
     # go back to the addon detail page on AMO to Enable the addon
     selenium.switch_to.window(selenium.window_handles[0])
@@ -80,7 +80,7 @@ def test_enable_disable_extension(
     # open the manage Extensions page in about:addons to verify that the extension was re-enabled
     selenium.switch_to.window(selenium.window_handles[1])
     wait.until(
-        lambda _: about_addons.installed_addon_name[0].text == "aarafow-molla-mantinch"
+        lambda _: about_addons.installed_addon_name[0].text == "PermissionsOrder3"
     )
 
 
