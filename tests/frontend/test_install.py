17ee3acca7130d70f9b74688a2575a86013d8411
aschek <aschek@mozilla.com>
2024-03-18 14:05:33 +0200

    fix2

--- tests/frontend/test_install.py
+++ tests/frontend/test_install.py
@@ -88,10 +88,10 @@ def test_install_uninstall_theme_tc_id_C95591(
     selenium, base_url, firefox, firefox_notifications, wait
 ):
     """Open a theme detail page, install it and then uninstall it"""
-    selenium.get(f"{base_url}/addon/full-moon-kitty/")
+    selenium.get(f"{base_url}/addon/calico-cat-1/")
     addon = Detail(selenium, base_url).wait_for_page_to_load()
     amo_theme_name = addon.name
-    assert amo_theme_name == "Full Moon Kitty"
+    assert amo_theme_name == "Calico Cat 1"
     assert addon.is_compatible
     addon.install()
     firefox.browser.wait_for_notification(
@@ -202,10 +202,10 @@ def test_about_addons_install_extension(
     wait.until(
         lambda _: len([el.install_button for el in about_addons.addon_cards_items]) >= 8
     )
-    disco_addon_name = about_addons.addon_cards_items[2].disco_addon_name.text
-    disco_addon_author = about_addons.addon_cards_items[2].disco_addon_author.text
+    disco_addon_name = about_addons.addon_cards_items[1].disco_addon_name.text
+    disco_addon_author = about_addons.addon_cards_items[1].disco_addon_author.text
     # install the recommended extension
-    about_addons.addon_cards_items[2].install_button.click()
+    about_addons.addon_cards_items[1].install_button.click()
     firefox.browser.wait_for_notification(
         firefox_notifications.AddOnInstallConfirmation
     ).install()
@@ -238,13 +238,13 @@ def test_about_addons_install_theme(
     wait.until(
         lambda _: len([el.install_button for el in about_addons.addon_cards_items]) >= 8
     )
-    disco_theme_name = about_addons.addon_cards_items[3].disco_addon_name.text
+    disco_theme_name = about_addons.addon_cards_items[0].disco_addon_name.text
     # make a note of the image source of the theme we are about to install
-    disco_theme_image = about_addons.addon_cards_items[3].theme_image.get_attribute(
+    disco_theme_image = about_addons.addon_cards_items[0].theme_image.get_attribute(
         "src"
     )
     # install the recommended theme
-    about_addons.addon_cards_items[3].install_button.click()
+    about_addons.addon_cards_items[0].install_button.click()
     firefox.browser.wait_for_notification(
         firefox_notifications.AddOnInstallConfirmation
     ).install()
