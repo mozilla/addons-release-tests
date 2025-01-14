ff213779163b5fb96f4c0ea8e75178e228bd4e09
aschek <aschek@mozilla.com>
2025-01-14 16:14:16 +0200

    skip, later some logging will be added for circle ci

--- tests/devhub/test_devhub_home.py
+++ tests/devhub/test_devhub_home.py
@@ -9,6 +9,7 @@ from scripts import reusables
 
 @pytest.mark.sanity
 @pytest.mark.nondestructive
+@pytest.mark.skip
 def test_devhub_page_get_involved(selenium, base_url, variables):
     page = DevHubHome(selenium, base_url).open().wait_for_page_to_load()
     # checks the content in the page 'Get Involved' - secondary section
