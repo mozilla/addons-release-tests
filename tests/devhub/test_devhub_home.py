68be7a9423e2155a1b7b8124228a6e07e12590f6
aschek <aschek@mozilla.com>
2025-06-25 13:06:40 +0300

    footer links

--- tests/devhub/test_devhub_home.py
+++ tests/devhub/test_devhub_home.py
@@ -481,7 +481,7 @@ def test_devhub_browsers_footer_links(base_url, selenium, count, link):
     "count, link",
     enumerate(
         [
-            "firefox/new/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=footer-link",
+            "firefox/new/?utm_campaign",
             "products/vpn/",
             "relay.firefox.com/",
             "monitor.mozilla",
