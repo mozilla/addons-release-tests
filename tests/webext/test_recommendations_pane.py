f54f2e864bdba329c10af5c29be208afd855551a
aschek <aschek@mozilla.com>
2026-01-30 13:58:18 +0200

    extensions

--- tests/webext/test_recommendations_pane.py
+++ tests/webext/test_recommendations_pane.py
@@ -9,7 +9,7 @@ from pages.desktop.about_addons import AboutAddons
 from pages.desktop.frontend.versions import Versions
 from pages.desktop.toolbar.toolbar import Toolbar
 
-@pytest.mark.webextrp
+@pytest.mark.webext
 def test_install_addon_from_recommendations_pane_TC_ID_C617016(selenium, base_url, firefox, firefox_notifications, variables, wait):
     selenium.get("about:addons")
     about_addons = AboutAddons(selenium, base_url).wait_for_page_to_load()
@@ -22,11 +22,8 @@ def test_install_addon_from_recommendations_pane_TC_ID_C617016(selenium, base_ur
     firefox.browser.wait_for_notification(
         firefox_notifications.AddOnInstallComplete
     ).close()
-    wait.until(
-        about_addons.addon_cards_items[1].manage_addon_button.is_displayed()
-    )
-    assert about_addons.addon_cards_items[1].manage_addon_button.is_displayed()
-    about_addons.addon_cards_items[1].manage_addon_button.click()
+    if len(selenium.window_handles) == 2:
+        selenium.switch_to.window(selenium.window_handles[0])
 
 
 
