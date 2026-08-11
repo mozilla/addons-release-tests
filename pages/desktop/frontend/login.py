6aeda217ef90ee4045317c77292eac15bc17896c
aschek <139126091+alexandruschek@users.noreply.github.com>
2026-08-12 00:21:18 +0300

    fix_user_and_devhub_tests (#1206)
    * fix_user_and_devhub_tests
    
    * add updates for contribute test

--- pages/desktop/frontend/login.py
+++ pages/desktop/frontend/login.py
@@ -66,14 +66,17 @@ class Login(Base):
 
     _email_locator = (By.NAME, "email")
     _continue_locator = (By.CSS_SELECTOR, ".button-row button")
-    _password_login = (By.CSS_SELECTOR, ".pb-1")
+    # every FxA screen renders its single input with the same '.pb-1' class, so
+    # the fields are matched by name to make sure we wait for the screen we
+    # actually need instead of matching the input of the previous one
+    _password_login = (By.CSS_SELECTOR, "input[name='password']")
     _password_locator = (By.XPATH, "//input[@data-testid='new-password-input-field']")
     _login_btn_locator = (By.CSS_SELECTOR, "button.cta-primary.cta-xl")
     _repeat_password_locator = (By.CSS_SELECTOR, "div.relative:nth-child(3) > div:nth-child(1) > label:nth-child(1) > span:nth-child(1) > input:nth-child(2)")
     _age_locator = (By.XPATH, "//input[@data-testid='age-input-field']")
     _code_input_locator = (By.CSS_SELECTOR, ".pb-1")
     _login_card_header_locator = (By.CSS_SELECTOR, ".card-header")
-    _2fa_input_locator = (By.CSS_SELECTOR, ".pb-1")
+    _2fa_input_locator = (By.CSS_SELECTOR, "input[name='code']")
     _confirm_2fa_button_locator = (By.CSS_SELECTOR, ".cta-primary")
     _error_2fa_code_locator = (By.CSS_SELECTOR, ".text-xs")
 
